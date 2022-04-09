440080984cface84c944c48c261616db2354354e
daszybak <karlo1diidd@gmail.com>
2022-04-09 14:14:19 +0200

    added useCallback hook

--- src/components/movie-container/movie-container.component.jsx
+++ src/components/movie-container/movie-container.component.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 
 import movies from '../../api/movies';
 import MoviesList from './movie-list.component';
@@ -10,23 +10,22 @@ const MovieContainer = ({term}) => {
   const [movieList, setMovieList] = useState([]);
   const initialRender = useRef(true);
 
+  const getMovies = useCallback(async () => {
+    const response = await movies.get('search/movie', {
+      params: {
+        query: term,
+      },
+    });
+    setMovieList(response.data.results);
+  }, [term]);
+
   useEffect(() => {
     if (initialRender.current) {
       initialRender.current = false;
       return;
     }
-    const getMovies = async () => {
-      const response = await movies.get('search/movie', {
-        params: {
-          query: term,
-        },
-      });
-      console.log(term);
-      console.log(response);
-      setMovieList(response.data.results);
-    };
     getMovies();
-  }, [term]);
+  }, [getMovies]);
 
   return (
     <>
